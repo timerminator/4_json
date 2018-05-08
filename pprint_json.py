5e710afd32a42d6567a060ffa5682d7dfdd8fc6b
timerminator <timerbaevruslan@gmail.com>
2018-05-08 13:49:19 +0500

    Add files via upload
    Исправлены названия переменных

--- pprint_json.py
+++ pprint_json.py
@@ -2,14 +2,14 @@ import json
 import pprint
 
 
-def load_data(filepath):
+def load_json(filepath):
     with open(filepath, encoding='utf-8') as json_content:
         return json.load(json_content)
 
 
-def pretty_print_json(data):
-    pprint.pprint(data)
+def pretty_print_json(json_data):
+    pprint.pprint(json_data)
 
 
 if __name__ == '__main__':
-    pretty_print_json(load_data(input("Input path: ")))
+    pretty_print_json(load_json(input("Input path: ")))
