7cfa67dbad2c1c03fbc7968c5e160d27e87276b6
timerminator <timerbaevruslan@gmail.com>
2018-05-08 17:09:17 +0500

    Add files via upload
    Исправлены ошибки

--- pprint_json.py
+++ pprint_json.py
@@ -1,15 +1,21 @@
 import json
-import pprint
-
+import sys
 
 def load_json(filepath):
-    with open(filepath, encoding='utf-8') as json_content:
-        return json.load(json_content)
+    with open(filepath, encoding='utf-8') as file:
+        return json.load(file)
 
 
-def pretty_print_json(json_data):
-    pprint.pprint(json_data)
+def pretty_print_json(data):
+    print(json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pretty_print_json(load_json(input("Input path: ")))
+    try:
+        pretty_print_json(load_json(sys.argv[1]))
+    except IndexError as e:
+        print('Вы не указали путь к файлу')
+    except IOError as e:
+        print('Не удалось открыть файл')
+    except json.decoder.JSONDecodeError:
+        print('Указанный файл не в формате json')
